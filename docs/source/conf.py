597eae166824045746b796b32b3893aa41645678
Devakumar Thammisetty <devakumar.thammisetty@epfl.ch>
2020-06-19 13:13:22 +0200

    Updated documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ release = "0.0.2"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["rinoh.frontend.sphinx", "sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
