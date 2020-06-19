0c0675c94961a2cc49d22497acf7030373e8d24e
Devakumar Thammisetty <devakumar.thammisetty@epfl.ch>
2020-06-19 03:01:44 +0200

    Added test cases in pytests and coverage reports

--- examples/moon_lander.py
+++ examples/moon_lander.py
@@ -1,7 +1,7 @@
 #
 # Copyright (c) 2020 LA EPFL.
 #
-# This file is part of MPOPT 
+# This file is part of MPOPT
 # (see http://github.com/mpopt).
 #
 # This program is free software: you can redistribute it and/or modify
@@ -21,6 +21,7 @@
 Created: 5th May 2020
 Author : Devakumar Thammisetty
 """
+from context import mpopt
 from mpopt import mp
 
 ocp = mp.OCP(n_states=2, n_controls=1)
--- tests/test_mpopt.py
+++ tests/test_mpopt.py
@@ -1,8 +1,7 @@
-#  -*- coding: utf-8 -*-
 #
 # Copyright (c) 2020 LA EPFL.
 #
-# This file is part of MPOPT 
+# This file is part of MPOPT
 # (see http://github.com/mpopt).
 #
 # This program is free software: you can redistribute it and/or modify
@@ -110,8 +109,217 @@ def test_ocp():
 
 
 @pytest.fixture
+def moon_lander_ocp():
+    ocp = mp.OCP(n_states=2, n_controls=1)
+
+    def dynamics0(x, u, t):
+        return [x[1], u[0] - 1.5]
+
+    ocp.dynamics[0] = dynamics0
+
+    def running_cost0(x, u, t):
+
+        return u[0]
+
+    ocp.running_costs[0] = running_cost0
+
+    def terminal_constraints0(xf, tf, x0, t0):
+
+        return [xf[0], xf[1]]
+
+    ocp.terminal_constraints[0] = terminal_constraints0
+
+    ocp.tf0[0] = 4.0
+    ocp.x00[0] = [10.0, -2.0]
+    ocp.lbx[0] = [-20.0, -20.0]
+    ocp.ubx[0] = [20.0, 20.0]
+    ocp.lbu[0] = 0
+    ocp.ubu[0] = 3
+    ocp.lbtf[0], ocp.ubtf[0] = 3, 5
+
+    ocp.validate()
+
+    return ocp
+
+
+@pytest.fixture
+def hyper_sensitive_ocp():
+    ocp = mp.OCP(n_states=1, n_controls=1, n_phases=1)
+
+    ocp.dynamics[0] = lambda x, u, t: [-x[0] * x[0] * x[0] + u[0]]
+    ocp.running_costs[0] = lambda x, u, t: 0.5 * (x[0] * x[0] + u[0] * u[0])
+    ocp.terminal_constraints[0] = lambda xf, tf, x0, t0: [xf[0] - 1.0]
+
+    ocp.x00[0] = 1
+    ocp.lbtf[0] = ocp.ubtf[0] = 1000.0
+    ocp.scale_t = 1 / 1000.0
+
+    ocp.validate()
+
+    return ocp
+
+
+@pytest.fixture
+def two_phase_schwartz_ocp():
+    ocp = mp.OCP(n_states=2, n_controls=1, n_phases=2)
+
+    # Step-1 : Define dynamics
+    def dynamics0(x, u, t):
+        return [x[1], u[0] - 0.1 * (1.0 + 2.0 * x[0] * x[0]) * x[1]]
+
+    ocp.dynamics = [dynamics0, dynamics0]
+
+    def path_constraints0(x, u, t):
+        return [
+            1.0
+            - 9.0 * (x[0] - 1) * (x[0] - 1)
+            - (x[1] - 0.4) * (x[1] - 0.4) / (0.3 * 0.3)
+        ]
+
+    ocp.path_constraints[0] = path_constraints0
+
+    def terminal_cost1(xf, tf, x0, t0):
+        return 5 * (xf[0] * xf[0] + xf[1] * xf[1])
+
+    ocp.terminal_costs[1] = terminal_cost1
+
+    ocp.x00[0] = [1, 1]
+    ocp.x00[1] = [1, 1]
+    ocp.xf0[0] = [1, 1]
+    ocp.xf0[1] = [0, 0]
+    ocp.lbx[0][1] = -0.8
+
+    ocp.lbu[0], ocp.ubu[0] = -1, 1
+
+    ocp.lbt0[0], ocp.ubt0[0] = 0, 0
+    ocp.lbtf[0], ocp.ubtf[0] = 1, 1
+    ocp.lbtf[1], ocp.ubtf[1] = 2.9, 2.9
+
+    ocp.validate()
+
+    return ocp
+
+
+@pytest.fixture
+def van_der_pol_ocp():
+    ocp = mp.OCP(n_states=2, n_controls=1)
+
+    def dynamics(x, u, t):
+        return [(1 - x[1] * x[1]) * x[0] - x[1] + u[0], x[0]]
+
+    def running_cost(x, u, t):
+        return x[0] * x[0] + x[1] * x[1] + u[0] * u[0]
+
+    ocp.dynamics[0] = dynamics
+    ocp.running_costs[0] = running_cost
+
+    ocp.x00[0] = [0, 1]
+    ocp.lbu[0] = -1.0
+    ocp.ubu[0] = 1.0
+    ocp.lbx[0][1] = -0.25
+    ocp.lbtf[0] = 10.0
+    ocp.ubtf[0] = 10.0
+
+    ocp.validate()
+
+    return ocp
+
+
+@pytest.fixture
 def test_mpo(test_ocp):
     mpo = mp.mpopt(test_ocp)
+    mpo.validate()
+
+    return mpo
+
+
+@pytest.fixture
+def moon_lander_mpo(moon_lander_ocp):
+    mpo = mp.mpopt(moon_lander_ocp, 20, 3)
+    mpo.validate()
+
+    return mpo
+
+
+@pytest.fixture
+def moon_lander_mpo_h_adaptive(moon_lander_ocp):
+    mpo = mp.mpopt_h_adaptive(moon_lander_ocp, 10, 4)
+    mpo.validate()
+
+    return mpo
+
+
+@pytest.fixture
+def moon_lander_mpo_adaptive(moon_lander_ocp):
+    mpo = mp.mpopt_adaptive(moon_lander_ocp, 3, 3)
+    mpo.lbh[0] = 1e-6
+    mpo.mid_residuals = True
+    mpo.validate()
+
+    return mpo
+
+
+@pytest.fixture
+def hyper_sensitive_mpo_h_adaptive(hyper_sensitive_ocp):
+    mpo = mp.mpopt_h_adaptive(hyper_sensitive_ocp, 15, 15)
+    mpo.validate()
+
+    return mpo
+
+
+@pytest.fixture
+def hyper_sensitive_mpo_adaptive(hyper_sensitive_ocp):
+    mpo = mp.mpopt_adaptive(hyper_sensitive_ocp, 5, 15)
+    mpo.lbh[0] = 1e-6
+    mpo.mid_residuals = True
+    mpo.validate()
+
+    return mpo
+
+
+@pytest.fixture
+def hyper_sensitive_mpo(hyper_sensitive_ocp):
+    mpo = mp.mpopt(hyper_sensitive_ocp, 15, 15)
+    mpo.validate()
+
+    return mpo
+
+
+@pytest.fixture
+def two_phase_schwartz_mpo(two_phase_schwartz_ocp):
+    mpo = mp.mpopt(two_phase_schwartz_ocp, 1, 15)
+    mpo.validate()
+
+    return mpo
+
+
+@pytest.fixture
+def van_der_pol_mpo(van_der_pol_ocp):
+    mpo = mp.mpopt(van_der_pol_ocp, 1, 15, "LGR")
+    mpo.validate()
+
+    return mpo
+
+
+@pytest.fixture
+def van_der_pol_mpo_lgl(van_der_pol_ocp):
+    mpo = mp.mpopt(van_der_pol_ocp, 1, 15, "LGL")
+    mpo.validate()
+
+    return mpo
+
+
+@pytest.fixture
+def van_der_pol_mpo_cgl(van_der_pol_ocp):
+    mpo = mp.mpopt(van_der_pol_ocp, 1, 15, "CGL")
+    mpo.validate()
+
+    return mpo
+
+
+@pytest.fixture
+def van_der_pol_mpo_h_adaptive(van_der_pol_ocp):
+    mpo = mp.mpopt_h_adaptive(van_der_pol_ocp, 5, 5, "LGR")
     mpo.validate()
 
     return mpo
@@ -195,3 +403,186 @@ def test_mpopt_solve(test_mpo):
     test_mpo.solve()
     for key in ["lbx", "lbg", "ubx", "ubg"]:
         assert key in test_mpo.nlp_bounds
+
+
+def test_moon_lander_mpopt_solve(moon_lander_mpo):
+    sol = moon_lander_mpo.solve()
+    for key in ["x", "f"]:
+        assert key in sol
+    post = moon_lander_mpo.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_moon_lander_h_adaptive_solve(moon_lander_mpo_h_adaptive):
+    sol = moon_lander_mpo_h_adaptive.solve(
+        max_iter=3, mpopt_options={"method": "residual", "sub_method": "merge_split"}
+    )
+    for key in ["x", "f"]:
+        assert key in sol
+    post = moon_lander_mpo_h_adaptive.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_moon_lander_h_adaptive2_solve(moon_lander_mpo_h_adaptive):
+    sol = moon_lander_mpo_h_adaptive.solve(
+        max_iter=2, mpopt_options={"method": "residual", "sub_method": "equal_area"}
+    )
+    for key in ["x", "f"]:
+        assert key in sol
+    post = moon_lander_mpo_h_adaptive.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_moon_lander_h_adaptive3_solve(moon_lander_mpo_h_adaptive):
+    sol = moon_lander_mpo_h_adaptive.solve(
+        max_iter=10, mpopt_options={"method": "control_slope", "sub_method": ""}
+    )
+    for key in ["x", "f"]:
+        assert key in sol
+    post = moon_lander_mpo_h_adaptive.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_moon_lander_mpopt_adaptive_solve(moon_lander_mpo_adaptive):
+    sol = moon_lander_mpo_adaptive.solve()
+    for key in ["x", "f"]:
+        assert key in sol
+    post = moon_lander_mpo_adaptive.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_hyper_sensitive_mpopt_solve(hyper_sensitive_mpo):
+    sol = hyper_sensitive_mpo.solve()
+    for key in ["x", "f"]:
+        assert key in sol
+    post = hyper_sensitive_mpo.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_hyper_sensitive_h_adaptive_solve(hyper_sensitive_mpo_h_adaptive):
+    sol = hyper_sensitive_mpo_h_adaptive.solve(
+        max_iter=3, mpopt_options={"method": "residual", "sub_method": "merge_split"}
+    )
+    for key in ["x", "f"]:
+        assert key in sol
+    post = hyper_sensitive_mpo_h_adaptive.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_hyper_sensitive_h_adaptive2_solve(hyper_sensitive_mpo_h_adaptive):
+    sol = hyper_sensitive_mpo_h_adaptive.solve(
+        max_iter=2, mpopt_options={"method": "residual", "sub_method": "equal_area"}
+    )
+    for key in ["x", "f"]:
+        assert key in sol
+    post = hyper_sensitive_mpo_h_adaptive.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_hyper_sensitive_h_adaptive3_solve(hyper_sensitive_mpo_h_adaptive):
+    sol = hyper_sensitive_mpo_h_adaptive.solve(
+        max_iter=10, mpopt_options={"method": "control_slope", "sub_method": ""}
+    )
+    for key in ["x", "f"]:
+        assert key in sol
+    post = hyper_sensitive_mpo_h_adaptive.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_hyper_sensitive_mpopt_adaptive_solve(hyper_sensitive_mpo_adaptive):
+    sol = hyper_sensitive_mpo_adaptive.solve()
+    for key in ["x", "f"]:
+        assert key in sol
+    post = hyper_sensitive_mpo_adaptive.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_two_phase_schwartz_mpopt_solve(two_phase_schwartz_mpo):
+    sol = two_phase_schwartz_mpo.solve()
+    for key in ["x", "f"]:
+        assert key in sol
+    post = two_phase_schwartz_mpo.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_van_der_pol_mpopt_solve(van_der_pol_mpo):
+    sol = van_der_pol_mpo.solve()
+    for key in ["x", "f"]:
+        assert key in sol
+    post = van_der_pol_mpo.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_van_der_pol_mpopt_solve(van_der_pol_mpo_lgl):
+    sol = van_der_pol_mpo_lgl.solve()
+    for key in ["x", "f"]:
+        assert key in sol
+    post = van_der_pol_mpo_lgl.process_results(sol, plot=False)
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
+
+
+def test_van_der_pol_mpopt_solve(van_der_pol_mpo_cgl):
+    sol = van_der_pol_mpo_cgl.solve()
+    for key in ["x", "f"]:
+        assert key in sol
+    post = van_der_pol_mpo_cgl.process_results(sol, plot=False)
+    fig, axs = post.plot_phases()
+    fig, axs = post.plot_x()
+    fig, axs = post.plot_u()
+    x, u, t = post.get_data()
+    xi, ui, ti = post.get_data(interpolate=True)
+
+    assert x.shape[0] == u.shape[0] == t.shape[0]
+    assert xi.shape[0] == ui.shape[0] == ti.shape[0]
