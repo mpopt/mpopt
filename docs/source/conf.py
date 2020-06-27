7f0f477637bb0bda257ac0933774860a0a795828
Devakumar Thammisetty <devakumar.thammisetty@epfl.ch>
2020-06-27 18:06:17 +0530

    Updated documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2020, Devakumar THAMMISETTY, Colin Jones"
 author = "Devakumar THAMMISETTY, Colin Jones"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.2"
+release = "0.1.2"
 
 
 # -- General configuration ---------------------------------------------------
